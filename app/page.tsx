b2e021f633c1222e4d9e6d1091813a4615f49ca7
Frode Jakobsen <Frojak4@gmail.com>
2025-02-05 15:05:58 +0100

    xx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ export default function Home() {
     <div>
       <section>
         <div className="h-[90vh] w-full dark:bg-myblack  dark:bg-grid-white/[0.05] bg-grid-black/[0.1] relative flex flex-col items-center justify-center">
-          {/* Radial gradient for the container to give a faded look */}
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-myblack  [mask-image:radial-gradient(ellipse_at_center,transparent_5%,black)]"></div>
           <h1 className="md:text-7xl text-5xl font-bold text-center ">
             Reddit, but not really.
